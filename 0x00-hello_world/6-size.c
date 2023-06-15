2f9f7ac7d61bf81be833b8a1728f933c8a7859fc
ChiChimzy15 <chichimzy15@gmail.com>
2023-06-15 16:29:54 -0700

    s

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -5,6 +5,11 @@
  */
 int main(void)
 {
+size_t charSize = sizeof(char);
+size_t intSize = sizeof(int);
+size_t longIntSize = sizeof(long int);
+size_t longLongIntSize = sizeof(long long int);
+size_t floatSize = sizeof(float);
 printf("Size of a char: %lu byte(s)\n", sizeof(char));
 printf("Size of an int: %lu byte(s)\n", sizeof(int));
 printf("Size of a long int: %lu byte(s)\n", sizeof(long int));
