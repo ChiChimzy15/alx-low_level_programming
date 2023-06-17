cbc3e71636ad0ef32bf256a9a590215574567aab
ChiChimzy15 <chichimzy15@gmail.com>
2023-06-17 16:34:29 -0700

    d

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ * he numbers of base 16 in lowercase
+ * Return: always 0 (Success)
+ */
+
+int main(void)
+{
+	int n;
+	int m;
+
+	for (n = 48; n <= 57; n++)
+	{
+		putchar(n);
+	}
+	for (n = 97; m <= 102; m++)
+	{
+		putchar(m);
+	}
+	putchar('\n');
+	return (0);
+}
