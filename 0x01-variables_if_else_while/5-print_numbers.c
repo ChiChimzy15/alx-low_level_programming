fa1c7aca2b9b309694bf311cacab24a332e92074
ChiChimzy15 <chichimzy15@gmail.com>
2023-06-17 15:46:14 -0700

    e

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * Main - Entry
+ * Print all single number in base 10
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int n;
+
+	for (n = 0; n <= 9; n++)
+	{
+		printf("%d", n);
+	}
+	putchar('\n');
+	return (0);
+}
