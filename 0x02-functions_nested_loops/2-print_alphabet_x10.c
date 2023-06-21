3ab6cf5fc9c03e5f242c5d42e3fc1a9067024ec1
ChiChimzy15 <chichimzy15@gmail.com>
2023-06-21 12:09:42 -0700

    s

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - print the alphabet 10 times
+ *
+ * Return: Always 0 (Success)
+ */
+void print_alphabet_x10(void)
+{
+	int i;
+	int f;
+
+	for (f = 1; i <= 10; i++)
+	{
+		for (f = 97; f <= 122; f++)
+		{
+			_putchar(f);
+		}
+		_putchar('\n');
+	}
+}
